78e215ed2f67388e60c3c71c703aaefb144de4e4
huochengyan <929914120@qq.com>
2020-03-17 17:15:21 +0800

    init pro

---
+++ src/main/java/com/hcy/huo/HuoApplication.java
@@ -0,0 +1,13 @@
+package com.hcy.huo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hcy/huo/HuoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcy.huo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
